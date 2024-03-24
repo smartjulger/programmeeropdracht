05746ae0ef176f01d46a51c43e04eec64d6fd3d6
julius gerth <julger04@gmail.com>
2024-03-24 13:22:02 +0100

    klaar

--- javascript.js
+++ javascript.js
@@ -1,39 +1,44 @@
 let button = document.getElementById("myButton");
+
+// Keystroke event listener
 document.addEventListener('keydown', function(event) {
-  if (event.key === ' ' || event.key === 'Space') {
-    const result = Math.random() >= 0.5 ? "hoofd" : "munt";
-    document.getElementById("result").innerHTML = result;
-
-    let coinImage = document.getElementById("coinFlip");
-    let originalSrc = coinImage.src;
-
-    if (result === "hoofd") {
-      setTimeout(() => {
-        alert("je hebt hoofd opniew proberen?");
-      }, 1100);
-      //gif is van https://tenor.com/nl/view/coin-flip-flip-coin-gif-19747326
-      coinImage.src = "img/coin-flip-flip.gif";
-      setTimeout(() => {
-        changeBackground("white");
-        coinImage.src = originalSrc;
-      }, 1500);
-    } else {
-      setTimeout(() => {
-        alert("je hebt munt opniew proberen?");
-      }, 1100);
-      //gif is van https://tenor.com/nl/view/coin-flip-flip-coin-gif-19747326
-      coinImage.src = "img/coin-flip-flip.gif";
-      //reset de achtergrond weer en de gif
-      setTimeout(() => {
-        changeBackground("white");
-        //img terug naar orgineel is met behulp van blackbox gedaan prompt die gebruikt was: ik wil me gif weer terug naar orgineel krijgen
-        coinImage.src = originalSrc;
-      }, 1500);
-    }
+  if (event.key === 'a') { 
+    simulateButtonClick();
   }
 });
 
-//achtergrond kleur
+button.addEventListener('click', function() {
+  simulateButtonClick();
+});
+
+function simulateButtonClick() {
+  const result = Math.random() >= 0.5 ? "hoofd" : "munt";
+  document.getElementById("result").innerHTML = result;
+
+  let coinImage = document.getElementById("coinFlip");
+  let originalSrc = coinImage.src;
+
+  if (result === "hoofd") {
+    setTimeout(() => {
+      alert("je hebt hoofd opnieuw proberen?");
+    }, 1100);
+    coinImage.src = "img/coin-flip-flip.gif";
+    setTimeout(() => {
+      changeBackground("white");
+      coinImage.src = originalSrc;
+    }, 1500);
+  } else {
+    setTimeout(() => {
+      alert("je hebt munt opnieuw proberen?");
+    }, 1100);
+    coinImage.src = "img/coin-flip-flip.gif";
+    setTimeout(() => {
+      changeBackground("white");
+      coinImage.src = originalSrc;
+    }, 1500);
+  }
+}
+
 function changeBackground(color) {
   document.body.style.background = color;
 }
